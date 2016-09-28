4582d0725b1ab37e84408cfcc7524f4a8f2c4376
Chance Gibbs <kcg005@shsu.edu>
2016-09-28 08:30:06 -0500

    Biblical and LotR names

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Markov = require('./markov.js');
 const fs = require('fs');
 
 // const names = fs.readFileSync('greek_names.txt', 'utf-8').replace(/\r/g, '').split('\n');
-const names = fs.readFileSync('spanish_names.txt', 'utf-8').replace(/\r/g, '').split('\n');
+const names = fs.readFileSync('lotr_names.txt', 'utf-8').replace(/\r/g, '').split('\n');
 let m = Markov.markovReadChars(names);
 console.log(Markov.markovGenerate(m, 25));
