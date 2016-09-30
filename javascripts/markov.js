b6aa541173a33511347693833955ca9a284b254e
Chance Gibbs <kcg005@shsu.edu>
2016-09-30 09:14:35 -0500

    Element names, and an attempt to prevent locking up the browser

--- javascripts/markov.js
+++ javascripts/markov.js
@@ -26,7 +26,9 @@ markovGenerate = function markovGenerate(marObj, times, depth=2) {
   if (depth < 2) return;
   let words = [];
   let i = 0;
-  while (i < times) {
+  let j = 0;
+  while (j < times * 1000 && i < times) {
+    j++;
     let next = ''
     const start = marObj.starts[Math.floor(Math.random() * marObj.starts.length)];;
     // console.log(start);
@@ -50,10 +52,13 @@ markovGenerate = function markovGenerate(marObj, times, depth=2) {
       k = temp + next;
     }
     // depth + 1 is used because of the newline
-    if (!!s && s.length) {
+    if (!!s && s.length > depth * 2 + 1) {
       words.push(s.slice(0,-1));
       i++;
     }
+  }
+  if (j >= times * 1000) {
+    words.push("Failed after " + (times * 100).toString() + " times. Maybe try a bigger list of words?");
   }
   return words;
 }
