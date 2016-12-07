25ffaa073e46c5603db47fc7eab2be2e599e9ce5
Chance Gibbs <kcg005@shsu.edu>
2016-12-07 13:05:25 -0600

    removed website cruft

--- javascripts/index.js
+++
@@ -1,7 +0,0 @@
-const Markov = require('./markov.js');
-const fs = require('fs');
-
-// const names = fs.readFileSync('greek_names.txt', 'utf-8').replace(/\r/g, '').split('\n');
-const names = fs.readFileSync('lotr_names.txt', 'utf-8').replace(/\r/g, '').split('\n');
-let m = Markov.markovReadChars(names);
-console.log(Markov.markovGenerate(m, 25));
